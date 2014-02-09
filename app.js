9db13f59cae25a09bb920ecdcf8d9888f4d83492
abhinand.lingareddy <abhinand.lingareddy@kony.com>
2014-02-09 16:27:20 +0530

    corrected

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-var express = require('express');
-var app = express();
-var path = require('path');
-
-var clientDir = path.join(__dirname, 'client');
 
 
     res.sendfile('client/index.html');
 
-app.configure ( function() {
-	app.use(express.favicon());
-    app.use(express.logger('dev'));
-	app.use(express.static(clientDir));
-}
